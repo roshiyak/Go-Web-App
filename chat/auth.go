7f09b21431737d0763e362b9921603930b2485ea
roshiyak <yashiro.kentaro@icloud.com>
2019-03-30 19:10:45 +0900

    update
    how to get avatars

--- chat/auth.go
+++ chat/auth.go
@@ -11,7 +11,22 @@ import (
 	"github.com/stretchr/objx"
 
 	"github.com/stretchr/gomniauth"
+	gomniauthcommon "github.com/stretchr/gomniauth/common"
 )
+
+type ChatUser interface {
+	UniqueID() string
+	AvatarURL() string
+}
+
+type chatUser struct {
+	gomniauthcommon.User
+	uniqueID string
+}
+
+func (u chatUser) UniqueID() string {
+	return u.uniqueID
+}
 
 type authHabdler struct {
 	next http.Handler
@@ -59,23 +74,28 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 			log.Fatalln("認証プロバイダーの取得に失敗しました", provider, "-", err)
 		}
 
-		cred, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		creds, err :=
+			provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
 		if err != nil {
 			log.Fatalln("認証を完了できませんでした", provider, "-", err)
 		}
 
-		user, err := provider.GetUser(cred)
+		user, err := provider.GetUser(creds)
 		if err != nil {
-			log.Fatalln("ユーザーの取得に失敗しました", provider, "-", err)
+			log.Fatalln("ユーザーの取得に失敗しました", provider, "- ", err)
 		}
+		chatUser := &chatUser{User: user}
 		m := md5.New()
 		io.WriteString(m, strings.ToLower(user.Name()))
-		userID := fmt.Sprintf("%x", m.Sum(nil))
+		chatUser.uniqueID = fmt.Sprintf("%x", m.Sum(nil))
+		avatarURL, err := avatars.GetAvatarURL(chatUser)
+		if err != nil {
+			log.Fatalln("GetAvatarURLに失敗しました", "-", err)
+		}
 		authCookieValue := objx.New(map[string]interface{}{
-			"userid":     userID,
+			"userid":     chatUser.uniqueID,
 			"name":       user.Name(),
-			"avatar_url": user.AvatarURL(),
-			"email":      user.Email(),
+			"avatar_url": avatarURL,
 		}).MustBase64()
 		http.SetCookie(w, &http.Cookie{
 			Name:  "auth",
--- chat/main.go
+++ chat/main.go
@@ -17,6 +17,8 @@ import (
 	"github.com/stretchr/gomniauth"
 )
 
+var avatars Avatar = UseFileSystemAvatar
+
 type templateHandler struct {
 	once     sync.Once
 	filename string
